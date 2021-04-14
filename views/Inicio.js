297e1072adfa99ae5f81b3b3828f7f007c597fcd
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-04-13 21:52:39 -0600

    componentes para las vitas

---
+++ views/Inicio.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from 'react-native';
+
+
+export const Inicio = () => {
+    return (
+        <Text>Hola mundo</Text>
+    )
+}
+
+
+export default Inicio;
