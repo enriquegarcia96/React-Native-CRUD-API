20fccf5ec1348e06c1ad37f24a60c1192381fe6f
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-04-14 22:47:24 -0600

    estilos globales para mi APP

---
+++ styles/dist/global.dev.js
@@ -0,0 +1,25 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _reactNative = require("react-native");
+
+var globalStyles = _reactNative.StyleSheet.create({
+  contedor: {
+    flex: 1,
+    marginTop: 20,
+    marginHorizontal: '2.5%'
+  },
+  titulo: {
+    textAlign: 'center',
+    marginTop: 20,
+    marginBottom: 30,
+    fontSize: 30
+  }
+});
+
+var _default = globalStyles;
+exports["default"] = _default;
---
+++ styles/global.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native'
+
+
+
+/**
+ * Creo mi hoja de estilo de manera Global
+ */
+const globalStyles = StyleSheet.create({
+
+    contedor:{
+        flex:1,
+        marginTop: 20,
+        marginHorizontal: '2.5%'
+    },
+    titulo:{
+        textAlign: 'center',
+        marginTop: 20,
+        marginBottom: 30,
+        fontSize: 30
+    }
+
+})
+
+export default globalStyles;
